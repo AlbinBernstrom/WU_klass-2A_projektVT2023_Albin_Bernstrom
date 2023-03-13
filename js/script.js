fe4843e926778d6435c172d516523d3ff6d327f4
hoppcorn <105201522+hoppcorn@users.noreply.github.com>
2023-03-13 14:56:06 +0100

    Hamburgare added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const topBar = document.querySelector('.top');
+const middleBar = document.querySelector('.middle');
+const bottomBar = document.querySelector('.bottom');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('is-active');
+  
+  if (hamburger.classList.contains('is-active')) {
+    topBar.style.transform = 'rotate(45deg)';
+    middleBar.style.opacity = 0;
+    bottomBar.style.transform = 'rotate(-45deg)';
+  } else {
+    topBar.style.transform = 'rotate(0)';
+    middleBar.style.opacity = 1;
+    bottomBar.style.transform = 'rotate(0)';
+  }
+});
