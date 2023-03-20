ee3413745b78b00333b2562382cb503f9b4cd6a6
hoppcorn <105201522+hoppcorn@users.noreply.github.com>
2023-03-20 14:53:49 +0100

    fixade layout med wrapper och lite annat

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@ const bottomBar = document.querySelector('.bottom');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
   
-  if (hamburger.classList.contains('is-active')) {
-    topBar.style.transform = 'rotate(45deg)';
-    middleBar.style.opacity = 0;
-    bottomBar.style.transform = 'rotate(-45deg)';
-  } else {
-    topBar.style.transform = 'rotate(0)';
-    middleBar.style.opacity = 1;
-    bottomBar.style.transform = 'rotate(0)';
-  }
+  // if (hamburger.classList.contains('is-active')) {
+  //   topBar.style.transform = 'rotate(45deg)';
+  //   middleBar.style.opacity = 0;
+  //   bottomBar.style.transform = 'rotate(-45deg)';
+  // } else {
+  //   topBar.style.transform = 'rotate(0)';
+  //   middleBar.style.opacity = 1;
+  //   bottomBar.style.transform = 'rotate(0)';
+  // }
 });
