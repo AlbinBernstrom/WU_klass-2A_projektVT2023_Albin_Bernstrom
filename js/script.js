84c1cee7a852a54c56586021ea898603d8115287
hoppcorn <105201522+hoppcorn@users.noreply.github.com>
2023-04-02 12:19:25 +0200

    Jag lade till faq, och lade till en separator som går diagonalt mella what och article

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
+const faq_1 = document.querySelector('faq');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('is-active');
